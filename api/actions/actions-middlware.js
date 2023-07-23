b6362e15b43cd86625fdb39ab0df68405156014a
Emre <ozturkemre@mail.com>
2023-07-23 23:18:36 +0300

    s13 challange tamamlandı

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,39 @@
 // eylemlerle ilgili ara katman yazılımları yazın
+const actionsModel = require("./actions-model");
+const projectModel = require("../projects/projects-model")
+
+async function validateActionId(req,res,next) {
+    try {
+        let existAction = await actionsModel.get(req.params.id);
+        if(!existAction){
+            res.status(404).json({message:"ACtions not found"})
+        }else{
+            req.existAction = existAction;
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
+}
+
+async function validateActionPayload(req,res,next){
+    try {
+        let {notes,description,project_id} = req.body;
+        if(!notes || !description || !project_id){
+            res.status(400).json({message:"Alanları kontrol ediniz."})
+        }else {
+            const existProject = await projectModel.get(project_id)
+            if(!existProject){
+                res.status(400).json({message:"Alanları kontrol ediniz."})
+            }else {
+                next()
+            }
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+module.exports = {
+    validateActionId,
+    validateActionPayload
+}
